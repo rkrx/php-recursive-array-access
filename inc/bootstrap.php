dc2e2dd4999bcefea99e6999305574a4a59f33ed
ron <ron.kirschler@gmail.com>
2014-05-12 11:03:13 +0200

    Minor changes to tests.xml and composer.json

--- inc/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-require __DIR__.'/../composer/autoload.php';
-require __DIR__.'/../src/Kir/Data/Arrays/RecursiveAccessor/ArrayPath/Accessor.php';
-require __DIR__.'/../src/Kir/Data/Arrays/RecursiveAccessor/StringPath/Accessor.php';
